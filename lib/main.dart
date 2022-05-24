6902330fc94c6b7602f7324210df6548d2c94acf
hghasemzadeh38@gmail.com <hghasemzadeh38@gmail.com>
2022-05-24 14:12:26 +0430

    new files added to views

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'src/core/utils/constants.dart';
 import 'src/injector.dart';
 
 void main() {
@@ -14,30 +13,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Todo app',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MainPage(),
-    );
-  }
-}
-
-class MainPage extends StatefulWidget {
-  const MainPage({Key? key}) : super(key: key);
-
-  @override
-  State<MainPage> createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(kMaterialAppTitle),
-      ),
-      body: Container(),
+      home: const Text('f'),
     );
   }
 }
