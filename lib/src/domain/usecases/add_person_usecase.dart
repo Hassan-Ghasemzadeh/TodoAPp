cb93e9d9db7c67ca6a1b6678813eaaff707f3d0a
hghasemzadeh38@gmail.com <hghasemzadeh38@gmail.com>
2022-05-25 08:49:54 +0430

    new file added

--- lib/src/domain/usecases/add_person_usecase.dart
+++ lib/src/domain/usecases/add_person_usecase.dart
@@ -1,7 +1,6 @@
 import 'package:hemend/object_controllers/data_snap_handler/data_snap_handler.dart';
-import 'package:todoappflutter/src/domain/repositories/base_person_impl.dart';
-
 import '../../data/models/person.dart';
+import '../repositories/base_person_impl.dart';
 
 class AddPersonUseCase {
   final PersonRepositoryImpl model;
